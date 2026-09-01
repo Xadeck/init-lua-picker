30ae150d3a5b7270a83f7f026078ab61adf63fdf
Xavier Décoret <475285+Xadeck@users.noreply.github.com>
2026-09-01 14:51:37 +0200

    Include base Tree-sitter scopes and definitions in queries/lua/locals.scm

--- queries/lua/locals.scm
+++ queries/lua/locals.scm
@@ -1,5 +1,61 @@
 ;; extends
 
+; Scopes
+[
+  (chunk)
+  (do_statement)
+  (while_statement)
+  (repeat_statement)
+  (if_statement)
+  (for_statement)
+  (function_declaration)
+  (function_definition)
+] @local.scope
+
+; Definitions
+(assignment_statement
+  (variable_list
+    (identifier) @local.definition.var))
+
+(assignment_statement
+  (variable_list
+    (dot_index_expression
+      .
+      (_) @local.definition.associated
+      (identifier) @local.definition.var)))
+
+((function_declaration
+  name: (identifier) @local.definition.function)
+  (#set! definition.function.scope "parent"))
+
+((function_declaration
+  name: (dot_index_expression
+    .
+    (_) @local.definition.associated
+    (identifier) @local.definition.function))
+  (#set! definition.method.scope "parent"))
+
+((function_declaration
+  name: (method_index_expression
+    .
+    (_) @local.definition.associated
+    (identifier) @local.definition.method))
+  (#set! definition.method.scope "parent"))
+
+(for_generic_clause
+  (variable_list
+    (identifier) @local.definition.var))
+
+(for_numeric_clause
+  name: (identifier) @local.definition.var)
+
+(parameters
+  (identifier) @local.definition.parameter)
+
+; References
+(identifier) @local.reference
+
+; Plugin Setup Blocks and Autocommands
 ((function_call
   name: [
     (dot_index_expression
@@ -19,3 +75,4 @@
     field: (identifier) @_autocmd (#eq? @_autocmd "nvim_create_autocmd"))) @local.definition.function
   (#set! definition.function.scope "parent")
   (#set! _init_lua_picker_setup true))
+
